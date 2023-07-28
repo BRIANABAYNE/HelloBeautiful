6483eeb89dae920169d90b4fd3dd9e5acafaa3f9
Briana Bayne <brianabayne@brianas-imac.lan>
2023-07-28 12:51:33 -0600

    Added another storyboard.

--- HelloBeautiful/LogIn/LogInViewModel.swift
+++ HelloBeautiful/LogIn/LogInViewModel.swift
@@ -6,31 +6,46 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+
+protocol LogInViewModelDelegate: LogInViewController {
+    func encountered(_ error: Error)
+}
 
 struct LogInViewModel {
     
     
     // MARK: - Properties
     
+    private let service: FirebaseServiceable
+    weak var delegate: LogInViewModelDelegate?
     
+   // MARK: - Dependency Injection
+    init(service: FirebaseServiceable = FirebaseService(), delegate: LogInViewModelDelegate) {
+        self.service = service
+        self.delegate = delegate
+    }
     
-    
-    //
-    //    func createAccount(with email: String, password: String, confirmPassword: String) {
-    //        if password == confirmPassword {
-    //            createAccount(with: email, password: password) { result in
-    //                switch result {
-    //                case.success(_):
-    //                    print("User was created successfully")
-    //                case.failure(let failure):
-    //
-    //
-    //                }
-    //            }
-    //
-    //        }
-    //
-    //    }
-    //
-    //}
-}
+  
+    func signIn(with email: String, password: String, confirmPassword: String ) {
+ 
+             if password == confirmPassword {
+                 service.signIn(email: email, password: password) { result  in
+                     switch result {
+                     case .success(_):
+                         print("User was created successfully")
+                     case .failure(let failure):
+                         delegate?.encountered(failure)
+                     }
+ 
+                 }
+ 
+ 
+             }
+             delegate?.encountered(CreateAccountError.passwordMismatch)
+ 
+         } // sign in
+         
+   
+} // LogIn
