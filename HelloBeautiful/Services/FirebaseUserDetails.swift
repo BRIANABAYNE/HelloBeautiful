cef3874d11aea4336210aac488ae6ed1c0a7ceb7
Briana Bayne <brianabayne@brianas-imac.lan>
2023-08-02 10:55:44 -0600

    FirebaseUserDetails

--- HelloBeautiful/Services/FirebaseUserDetails.swift
+++ HelloBeautiful/Services/FirebaseUserDetails.swift
@@ -5,69 +5,62 @@
 //  Created by Briana Bayne on 7/27/23.
 //
 
+import FirebaseFirestore
 import Foundation
+import FirebaseStorage
+import FirebaseFirestoreSwift
 
 
-// MARK: - Propeties
-var userDetails: UserDetails?
 
-func userDetails(zodiacSign: String, cycleLength:String, lastCycle: String, completion: @escaping(Result<String, Error>) -> Void) {
-    let userDetails = UserDetails(zodiacSign: zodiacSign, cycleLength: cycleLength, lastCycle: lastCycle, collectionType: Constants.UserDetails.userDetailsCollectionPath)
+struct FirebaseUerDetailsService {
     
+    
+    
+    func userDetails(zodiacSign: String, cycleLength:String, lastCycle: String, completion: @escaping(Result<String, Error>) -> Void) {
+        let userDetails = UserDetails(zodiacSign: zodiacSign, cycleLength: cycleLength, lastCycle: lastCycle, collectionType: Constants.UserDetails.userDetailsCollectionPath)
+        
+        save(userDetails: userDetails) { result in
+            switch result {
+            case .success(let docID):
+                completion(.success(docID))
+            case .failure(let failure):
+                print(failure)
+            }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var user: User?
-//
-//
-//func create(firstName: String, lastName: String, email: String, password: String, lastCycle: String, periodLength: Int, zodiacSign: String, completion: @escaping(Result<String,FirebaseError>) -> Void) {
-//    let user = User(firstName: firstName, lastName: lastName, email: email, password: password, zodiacSign: zodiacSign, lastCycle: lastCycle , collectionType: Constants.User.userCollectionPath)
-//}
-//
-//func updateUser() {
-//
-//}
-//
-//
-//
-//func update(user: User) {
-//    if let documentID = user.id {
-//        let ref = Firestore.firestore()
-//        let docRef = ref.collection(Constants.User.userCollectionPath).document(documentID)
-//
-//        do {
-//            try docRef.setData(from: user)
-//        } catch {
-//            print(error)
-//        }
-//    }
-//
-//
-//    func save() {
-//
-//    }
-//
-//    func fetchAllUsers() {
-//
-//    }
-//
-//    func deleteUser() {
-//
-//    }
-//}
+    } // end of user details
+    
+    func save(userDetails: UserDetails, completion: @escaping(Result<String, FirebaseError>) -> Void ) {
+        let ref = Firestore.firestore()
+        do {
+            let documentRef = try ref.collection(Constants.UserDetails.userDetailsCollectionPath).addDocument(from: userDetails, completion: { _ in
+                
+            })
+            completion(.success(documentRef.documentID))
+        } catch {
+            print("Oh no, something went wrong with the save", error.localizedDescription)
+            return
+        }
+    } // end of save
+    
+    func updateUser(newZodiacSign: String, newCycleLength: String, newLastCycle: String) {
+    
+        let updateUser = UserDetails(zodiacSign: newZodiacSign, cycleLength: newCycleLength, lastCycle: newLastCycle, collectionType:Constants.UserDetails.userDetailsCollectionPath)
+       
+        update(userDetails: updateUser)
+        
+    } // end of updateUser
+    
+    func update(userDetails: UserDetails) {
+        if let documentID = userDetails.id {
+            let ref = Firestore.firestore() // path to fireStore
+            let docRef = ref.collection(Constants.UserDetails.userDetailsCollectionPath).document(documentID) // collection, doc, collection, doc
+            
+            do {
+                try docRef.setData(from: userDetails)
+            } catch {
+                print(error)
+            }
+        }
+        
+    } // end of update
+} // end of struct 
